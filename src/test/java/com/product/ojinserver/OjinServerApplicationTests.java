35646d844be35cbdbb838c92214dfc8a96adf1cf
Quan VN <quanvuongngoc98@gmail.com>
2022-03-10 16:46:53 +0700

    init project

---
+++ src/test/java/com/product/ojinserver/OjinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.ojinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OjinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
